b8d0257034c72ffc25513b3fa1259ec9f38b53f1
dongheemin <mindh@gccorp.com>
2022-01-04 14:05:46 +0900

    220104
    report test2

--- report/src/main/kotlin/com/greencross/lims/report/avoid/kokr/AvoidResourceN201KoKr.kt
+++ report/src/main/kotlin/com/greencross/lims/report/avoid/kokr/AvoidResourceN201KoKr.kt
@@ -5,7 +5,7 @@ import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.pdmodel.font.PDFont
 import java.io.File
 
-class AvoidResourceN201KoKr(doc: PDDocument) : AvoidResourceKoKr, AvoidResourceN201 {
+class AvoidResourceN201KoKr(doc: PDDocument) : AvoidResourceKoKr(doc), AvoidResourceN201 {
     val fontDefault: PDFont
 
     val img: File = File(AvoidResourceN201, "img/Avoid/N201/KoKr")
