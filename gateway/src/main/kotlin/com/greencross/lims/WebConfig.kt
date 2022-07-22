3cdde858469102a19a3b3df5ccb53e15c1f0a8c6
최지수 <imbtmn@gccorp.com>
2022-07-22 15:31:08 +0900

    static file 위치 변경

--- gateway/src/main/kotlin/com/greencross/lims/WebConfig.kt
+++ gateway/src/main/kotlin/com/greencross/lims/WebConfig.kt
@@ -1,22 +1,18 @@
 package com.greencross.lims
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.cloud.client.loadbalancer.LoadBalanced
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.CacheControl
 import org.springframework.http.codec.ClientCodecConfigurer
 import org.springframework.http.codec.ServerCodecConfigurer
 import org.springframework.http.codec.json.Jackson2JsonDecoder
 import org.springframework.http.codec.json.Jackson2JsonEncoder
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.web.reactive.config.EnableWebFlux
-import org.springframework.web.reactive.config.ResourceHandlerRegistry
 import org.springframework.web.reactive.config.WebFluxConfigurer
 import org.springframework.web.reactive.function.client.ExchangeStrategies
 import org.springframework.web.reactive.function.client.WebClient
-import java.util.concurrent.TimeUnit
 
 
 @Configuration
@@ -25,8 +21,6 @@ import java.util.concurrent.TimeUnit
 open class WebConfig(
     private val objectMapper: ObjectMapper
     ) : WebFluxConfigurer {
-    @Value("\${server.resources}")
-    lateinit var resources: String
     @Bean
     @LoadBalanced
     open fun clientBuilder(): WebClient.Builder {
@@ -36,13 +30,6 @@ open class WebConfig(
                     configurer.defaultCodecs().jackson2JsonEncoder(Jackson2JsonEncoder(objectMapper))
                     configurer.defaultCodecs().jackson2JsonDecoder(Jackson2JsonDecoder(objectMapper))
                 }.build())
-    }
-    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
-        println(resources == "file:/data/lims/static/avoid-service/")
-        registry.addResourceHandler("/**")
-            .addResourceLocations(resources)
-            .setCacheControl(CacheControl.maxAge(1, TimeUnit.MINUTES))
-            .resourceChain(false)
     }
     override fun configureHttpMessageCodecs(configurer: ServerCodecConfigurer) {
         configurer.defaultCodecs().jackson2JsonEncoder(Jackson2JsonEncoder(objectMapper))
