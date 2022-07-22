cf80818896447d2023960b265c8d0137a7cde4e0
최지수 <imbtmn@gccorp.com>
2022-07-22 13:52:48 +0900

    불필요한 static file 핸들링 제거

--- analysis/src/main/kotlin/com/gcgenome/lims/WebConfig.kt
+++ analysis/src/main/kotlin/com/gcgenome/lims/WebConfig.kt
@@ -2,16 +2,13 @@ package com.gcgenome.lims
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.CacheControl
 import org.springframework.http.codec.ServerCodecConfigurer
 import org.springframework.http.codec.json.Jackson2JsonDecoder
 import org.springframework.http.codec.json.Jackson2JsonEncoder
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.web.reactive.config.CorsRegistry
 import org.springframework.web.reactive.config.EnableWebFlux
-import org.springframework.web.reactive.config.ResourceHandlerRegistry
 import org.springframework.web.reactive.config.WebFluxConfigurer
-import java.util.concurrent.TimeUnit
 
 @Configuration
 @EnableAsync
@@ -23,13 +20,6 @@ class WebConfig(private val objectMapper: ObjectMapper) : WebFluxConfigurer {
             .allowedMethods("GET", "POST" ,"PATCH", "PUT", "DELETE", "OPTIONS") // put the http verbs you want allow
             .allowedHeaders("X-USER-ID", "Content-Type") // put the http headers you want allow
             .exposedHeaders("X-Total-Count", "X-Total-Page", "X-Current-Page")
-    }
-
-    override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
-        registry.addResourceHandler("/**")
-            .addResourceLocations("classpath:/static/")
-            .setCacheControl(CacheControl.maxAge(1, TimeUnit.MINUTES))
-            .resourceChain(false)
     }
     override fun configureHttpMessageCodecs(configurer: ServerCodecConfigurer) {
         configurer.defaultCodecs().jackson2JsonEncoder(Jackson2JsonEncoder(objectMapper))
