1b73f475e9fdcd2609908957f9a82a3c3c47e39d
변상재 <sayaya@greencross.com>
2022-07-22 17:56:57 +0900

    preprocessing 필드 추가

--- worklist-api/src/main/kotlin/com/gcgenome/lims/service/preprocessing/PreprocessingHandler.kt
+++ worklist-api/src/main/kotlin/com/gcgenome/lims/service/preprocessing/PreprocessingHandler.kt
@@ -12,7 +12,7 @@ class PreprocessingHandler(val dao: PreprocessingDao) {
     fun save(dto : Preprocessing): Mono<Any> {
         val worklist = dto.id!!.split("$")[0]
         val index = dto.id.split("$")[1]
-        return dao.merge(worklist, index, dto.json!!)
+        return dao.merge(worklist, index, "")
     }
     fun saveMany(dtos: List<Preprocessing>) : Flux<Any> {
         return Flux.mergeSequential(dtos.map(this::save))
