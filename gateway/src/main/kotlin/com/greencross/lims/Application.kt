30b74a1a88982038e7c93d38ec2b8b7ea60b5076
변상재 <sayaya@greencross.com>
2022-08-14 10:27:06 +0900

    .

--- gateway/src/main/kotlin/com/greencross/lims/Application.kt
+++ gateway/src/main/kotlin/com/greencross/lims/Application.kt
@@ -1,31 +1,13 @@
 package com.greencross.lims
 
-import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler
-import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
-import org.springframework.scheduling.annotation.AsyncConfigurer
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
-import java.util.concurrent.Executor
 
 @SpringBootApplication
 // @EnableFeignClients
 @EnableDiscoveryClient
-open class Application : AsyncConfigurer {
-    override fun getAsyncExecutor(): Executor {
-        val executor = ThreadPoolTaskExecutor()
-        executor.corePoolSize = 5
-        executor.maxPoolSize = 30
-        executor.setQueueCapacity(10)
-        executor.initialize()
-        return executor
-    }
-
-    override fun getAsyncUncaughtExceptionHandler(): AsyncUncaughtExceptionHandler? {
-        return SimpleAsyncUncaughtExceptionHandler()
-    }
-
+open class Application {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
