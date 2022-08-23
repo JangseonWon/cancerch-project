924000af0f4dac115ad52d95d28c144e0c3dfee0
변상재 <sayaya@greencross.com>
2022-08-23 19:34:37 +0900

    .

--- sequencing-api/src/main/kotlin/com/gcgenome/lims/service/lims1/Batch.kt
+++ sequencing-api/src/main/kotlin/com/gcgenome/lims/service/lims1/Batch.kt
@@ -1,11 +1,12 @@
 package com.gcgenome.lims.service.lims1
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import java.util.*
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class Batch (
     val template: UUID = UUIDEnum.TEMPLATE.toUUID(),
-    val idx: Int = 0,
-    val state: String = "CREATE"
+    val idx: Int = 0
 ) {
     var title: String = ""
     val value: MutableMap<UUID, String> = mutableMapOf()
