a1b113f2b57d0ab05620d2fac08dc92f5353312e
dongheemin <mindh@gccorp.com>
2022-08-30 12:38:22 +0900

    .

--- report/src/main/kotlin/com/greencross/lims/service/report/ReportHandler.kt
+++ report/src/main/kotlin/com/greencross/lims/service/report/ReportHandler.kt
@@ -120,7 +120,7 @@ class ReportHandler(
         avoidDto.collectionDate = analysis.dateSampling.toLocalDate()
         avoidDto.receiptDate = analysis.dateRequest.toLocalDate()
         avoidDto.reportDate = LocalDate.now()
-        avoidDto.medicalRecordNumber = analysis.remark
+        avoidDto.medicalRecordNumber = analysis.patient.mrn
         avoidDto.barcode = barcode
         avoidDto.medicalInstitution = customerName
         avoidDto.medicalRecordNumber = patient.code
