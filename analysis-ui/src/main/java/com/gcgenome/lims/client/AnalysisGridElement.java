4db6a5e8fd50592261071a09cbdf765dd4893c00
dongheemin <mindh@gccorp.com>
2022-08-30 10:47:37 +0900

    .

--- analysis-ui/src/main/java/com/gcgenome/lims/client/AnalysisGridElement.java
+++ analysis-ui/src/main/java/com/gcgenome/lims/client/AnalysisGridElement.java
@@ -42,13 +42,13 @@ public class AnalysisGridElement extends HTMLElementBuilder<HTMLDivElement, Anal
 		}).color("#FFFFFF");
 	}
 	private static ColumnString columnThreshold(String name, Code code, String type){
-		return ColumnBuilder.string(name).width(100).name(name).readOnly(true).horizontal("center").colorBackground((td, row, prop, value) ->{
+		return ColumnBuilder.string(name).width(100).name(name).readOnly(true).horizontal("right").colorBackground((td, row, prop, value) ->{
 			if(code.equals(Code.FREEMIX)) {
-				if(Double.valueOf(value) >= 1.0) return "#FF0000";
+				if(Double.valueOf(value).doubleValue() >= 1.0) return "#FF0000";
 				else return type.equals("A") ? "#BF5C26" : "#8C7162";
 			}
 			else if(code.equals(Code.FINAL_READS)) {
-				if(Double.valueOf(value) <= 40.0) return "#FF0000";
+				if(Double.valueOf(value).doubleValue() <= 40.0) return "#FF0000";
 				else return type.equals("A") ? "#BF5C26" : "#8C7162";
 			}
 			else return type.equals("A") ? "#BF5C26" : "#8C7162";
