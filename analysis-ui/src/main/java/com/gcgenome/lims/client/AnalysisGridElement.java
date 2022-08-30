df71887a9c36b9ac7f2614baf7e7e648130ff35d
dongheemin <mindh@gccorp.com>
2022-08-30 14:05:22 +0900

    QC Fail 사유 컬럼 시각화

--- analysis-ui/src/main/java/com/gcgenome/lims/client/AnalysisGridElement.java
+++ analysis-ui/src/main/java/com/gcgenome/lims/client/AnalysisGridElement.java
@@ -43,21 +43,21 @@ public class AnalysisGridElement extends HTMLElementBuilder<HTMLDivElement, Anal
 		}).color("#FFFFFF");
 	}
 	private static ColumnString columnThreshold(String name, Code code, String type){
-//		return ColumnBuilder.string(name).width(100).name(name).readOnly(true).horizontal("right").colorBackground((td, row, prop, value) ->{
-//			if(code.equals(Code.FREEMIX) && !value.equals("null")) {
-//				if(Double.valueOf(value) >= 1.0) return "#FF0000";
-//				else return type.equals("A") ? "#BF5C26" : "#8C7162";
-//			}
-//			else if(code.equals(Code.FINAL_READS) && !value.equals("null")) {
-//				if(Double.valueOf(value) <= 40.0) return "#FF0000";
-//				else return type.equals("A") ? "#BF5C26" : "#8C7162";
-//			}
-//			else return type.equals("A") ? "#BF5C26" : "#8C7162";
-//		}).color("#FFFFFF");
 		return ColumnBuilder.string(name).width(100).name(name).readOnly(true).horizontal("right").colorBackground((td, row, prop, value) ->{
-			if("A".equals(type)) return "#BF5C26";
-			else return "#8C7162";
+			if(code.equals(Code.FREEMIX) && value != null) {
+				if(Double.valueOf(value) >= 1.0) return "#FF0000";
+				else return type.equals("A") ? "#BF5C26" : "#8C7162";
+			}
+			else if(code.equals(Code.FINAL_READS) && value != null) {
+				if(Double.valueOf(value) <= 40.0) return "#FF0000";
+				else return type.equals("A") ? "#BF5C26" : "#8C7162";
+			}
+			else return type.equals("A") ? "#BF5C26" : "#8C7162";
 		}).color("#FFFFFF");
+//		return ColumnBuilder.string(name).width(100).name(name).readOnly(true).horizontal("right").colorBackground((td, row, prop, value) ->{
+//			if("A".equals(type)) return "#BF5C26";
+//			else return "#8C7162";
+//		}).color("#FFFFFF");
 	}
 	private static ColumnString columnPassOrFail(String name){
 		return ColumnBuilder.string(name).width(100).name(name).readOnly(true).horizontal("center").colorBackground((td, row, prop, value) ->{
