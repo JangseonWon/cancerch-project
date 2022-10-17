68f7e9915b10d63ab84b36cdd0bd962ac1c680a5
easternpillar <jangdj@gccorp.com>
2022-10-17 17:30:31 +0900

    결과지 생성 Cron

--- report/src/main/kotlin/com/greencross/lims/projection/Report.kt
+++ report/src/main/kotlin/com/greencross/lims/projection/Report.kt
@@ -8,39 +8,43 @@ import java.util.*
 data class Report(
     val sample: Long,
     val service: String,
-    val file: UUID,
+    val file: UUID?,
     val createAt: LocalDateTime,
     val createBy: User,
     val lastModifyAt: LocalDateTime,
     val lastModifyBy: User,
-    val name: String,
-    val size: Long,
+    val name: String?,
+    val size: Long?,
     val publishAt: LocalDateTime?,
     val publishBy: User?,
-    val publishLog: String?
+    val publishLog: String?,
+    val isPrinted: String?,
+    val language: String?
 ) {
     companion object{
         data class ReportBuilder(
             val sample:         Long,
             val service:        String,
-            val file:           UUID,
+            val file:           UUID?,
             val createId:       String,
             val createAt:       LocalDateTime,
             val createBy:       String,
             val lastModifyId:   String,
             val lastModifyAt:   LocalDateTime,
             val lastModifyBy:   String,
-            val name:           String,
-            val size:           Long,
+            val name:           String?,
+            val size:           Long?,
             val publishId:      String?,
             val publishAt:      LocalDateTime?,
             val publishBy:      String?,
-            val publishLog:     Json?
+            val publishLog:     Json?,
+            val isPrinted:      String?,
+            val language:       String?
         ){
             fun build() : Report{
                 val publishUser : User? = if(publishId == null || publishBy == null) null else User(publishId, publishBy)
                 val log : String? = if(publishLog == null) "" else publishLog.asString()
-                return Report(sample, service, file, createAt, User(createId, createBy), lastModifyAt, User(lastModifyId, lastModifyBy), name, size, publishAt, publishUser, log)
+                return Report(sample, service, file, createAt, User(createId, createBy), lastModifyAt, User(lastModifyId, lastModifyBy), name, size, publishAt, publishUser, log, isPrinted, language)
             }
         }
     }
