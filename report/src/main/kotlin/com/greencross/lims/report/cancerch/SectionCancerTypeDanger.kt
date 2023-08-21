c972071c8ebe6c700060ab467f387ec8951c874e
dongheemin <mindh@gccorp.com>
2023-08-21 16:55:21 +0900

    fix(report) : 캔서치용 레포지토리 분리

--- report/src/main/kotlin/com/greencross/lims/report/cancerch/SectionCancerTypeDanger.kt
+++ report/src/main/kotlin/com/greencross/lims/report/cancerch/SectionCancerTypeDanger.kt
@@ -1,12 +1,11 @@
 package com.greencross.lims.report.cancerch
 
 import com.greencross.lims.report.TextBlock
-import com.greencross.lims.report.avoid.repository.CancerRepo
+import com.greencross.lims.report.cancerch.repository.CancerRepo
 import com.greencross.lims.report.builder.Sex
 import com.greencross.lims.report.func.AlignHorizontal
 import com.greencross.lims.report.func.PDPageContentStreamPageAccessible
 import com.greencross.lims.report.func.Painter
-import org.apache.kafka.common.protocol.types.Field.Bool
 import java.awt.Color
 import kotlin.math.round
 
