30ccca6b9d2d9932928cc7c244bf87997c8f8ad6
dongheemin <mindh@gccorp.com>
2024-08-09 17:18:30 +0900

    fix(report): Intro 작업

--- report/src/main/kotlin/com/greencross/lims/report/cancerch/GangbukComponent/GangbukSectionIntroEnUs.kt
+++ report/src/main/kotlin/com/greencross/lims/report/cancerch/GangbukComponent/GangbukSectionIntroEnUs.kt
@@ -26,9 +26,9 @@ class GangbukSectionIntroEnUs(private val y: Float = 685f): Painter<CancerchTemp
         width = img.width * INTRO_CONTENT_RATE / img.height
         stream.drawImage(img, 298f-width/2, y- CONTENT_TITLE_RATE -29, width, INTRO_CONTENT_RATE)
         stream.paragraph(48f, y-29, 500f, AlignHorizontal.LEFT, TextBlock(template.resource().styleContentRegualar().clone().fontSize(7f),
-            "Circulating tumor DNA test is the multi cancer early detection(MCED) test powered by artificial intelligence(AI) that has trained distinctive DNA patterns from approximately\n" +
-                "5,000 cancer patients and healthy individuals. It analyzes DNA patterns to predict the likelihood of the 6 major types of cancers. The test results indicate the\n" +
-                "potential presence of circulating tumor DNA and require further diagnostic confirmations."))
+            "Circulating tumor DNA test is the multi cancer early detection(MCED) test powered by artificial intelligence(AI) that has trained distinctive DNA patterns from\n" +
+                "approximately 5,000 cancer patients and healthy individuals. It analyzes DNA patterns to predict the likelihood of the 6 major types of cancers. The test results\n" +
+                "indicate the potential presence of circulating tumor DNA and require further diagnostic confirmations."))
 
         stream.restoreGraphicsState()
         return stream
