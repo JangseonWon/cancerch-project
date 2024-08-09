05118280caee919fd82a67764e2cdf15ef4c667e
dongheemin <mindh@gccorp.com>
2024-08-09 17:14:32 +0900

    fix(report): Intro 작업

--- report/src/main/kotlin/com/greencross/lims/report/cancerch/GangbukComponent/GangbukSectionIntroKoKr.kt
+++ report/src/main/kotlin/com/greencross/lims/report/cancerch/GangbukComponent/GangbukSectionIntroKoKr.kt
@@ -26,7 +26,7 @@ class GangbukSectionIntroKoKr(private val y: Float = 685f): Painter<CancerchTemp
         width = img.width * INTRO_CONTENT_RATE / img.height
         stream.drawImage(img, 298f-width/2, y- CONTENT_TITLE_RATE -29, width, INTRO_CONTENT_RATE)
         stream.paragraph(51f, y-32, 500f, AlignHorizontal.LEFT, TextBlock(template.resource().styleContentRegualar().clone().fontSize(9f),
-            "아이캔서치 검사는 약 5,000명의 암 환자 및 건강인에서 특징적으로 나타나는 DNA 패턴을 학습한 인공지능으로 수검자의 DNA\n" +
+            "종양 DNA 혈액검사는 약 5,000명의 암 환자 및 건강인에서 특징적으로 나타나는 DNA 패턴을 학습한 인공지능으로 수검자의 DNA\n" +
                 "패턴을 분석하여 주요 6종 암의 존재 가능성을 예측합니다. 본 검사의 결과는 암의 진단 혹은 완전한 배제를 의미하지 않습니다."))
 
         stream.restoreGraphicsState()
