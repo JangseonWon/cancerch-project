35116235c6a902bd341f6801c7fcefd47a74b013
dongheemin <mindh@gccorp.com>
2024-08-20 11:00:33 +0900

    fix(publish): 롤백

--- publish/src/main/kotlin/com/gcgenome/alis/Client.kt
+++ publish/src/main/kotlin/com/gcgenome/alis/Client.kt
@@ -59,18 +59,18 @@ class Client(
                 fileId = file.toString(),
                 operation = Operation.SEND_USER_FILE
             ),
-            AlisRequest(
-                fileId = file.toString(),
-                operation = Operation.SEND_RESULT,
-                results = listOf(
-                    AlisResult(
-                        request.service + "010",
-                        resultInfo.result,
-                        resultInfo.resultType,
-                        resultInfo.comment
-                    )
-                )
-            )
+//            AlisRequest(
+//                fileId = file.toString(),
+//                operation = Operation.SEND_RESULT,
+//                results = listOf(
+//                    AlisResult(
+//                        request.service + "010",
+//                        resultInfo.result,
+//                        resultInfo.resultType,
+//                        resultInfo.comment
+//                    )
+//                )
+//            )
         )
 
         if (request.institution!! == "G062546" || request.institution == "G075003" || request.institution == "G001125" || request.institution == "Q000003" || request.institution == "G0Q0006") {
