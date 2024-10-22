9c2d0896d058ec8e91fd9c204d9932afb6552e10
mjjuu <mjju@gcgenome.com>
2024-10-22 16:43:16 +0900

    fix(analysis-ui): maven 순서 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,10 @@ subprojects {
     repositories {
         mavenCentral()
         mavenLocal()
-        maven(url = "http://gemini/api/packages/LIMS/maven"){
+        maven(url = "http://gitea.apps.gcgenome.com/api/packages/LIMS/maven"){
             isAllowInsecureProtocol = true
         }
-        maven(url = "http://gitea.apps.gcgenome.com/api/packages/LIMS/maven"){
+        maven(url = "http://gemini/api/packages/LIMS/maven"){
             isAllowInsecureProtocol = true
         }
     }
