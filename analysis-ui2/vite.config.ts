b9d4d5f2b283a9d5d3d6258b51535e2ef60bda10
mjjuu <mjju@gcgenome.com>
2024-11-08 11:05:16 +0900

    fix(analysis-ui2): config 수정

--- analysis-ui2/vite.config.ts
+++ analysis-ui2/vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        app: './analysis.html'
+      }
+    }
+  },
+  base: "/analysis/",
+  server: {
+    open: '/analysis/analysis.html',
+  }
 })
