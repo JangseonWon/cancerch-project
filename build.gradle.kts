ebf27d359769ae3554ef7f6594e84e457d3f7b45
mjjuu <mjju@gcgenome.com>
2024-11-08 14:37:29 +0900

    fix(publish): jandi webhook 연동

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,12 @@ subprojects {
     repositories {
         mavenCentral()
         mavenLocal()
+        maven(url = "https://maven.pkg.github.com/GC-Genome/maven") {
+            credentials {
+                username = (project.findProperty("github_username") ?: System.getenv("GITHUB_USERNAME")).toString()
+                password = (project.findProperty("github_password") ?: System.getenv("GITHUB_TOKEN")).toString()
+            }
+        }
         maven(url = "http://gitea.apps.gcgenome.com/api/packages/LIMS/maven"){
             isAllowInsecureProtocol = true
         }
