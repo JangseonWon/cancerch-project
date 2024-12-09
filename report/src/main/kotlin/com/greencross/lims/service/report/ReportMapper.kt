e58a15184678f90619671850d6c327b9de997461
mjjuu <mjju@gcgenome.com>
2024-12-09 13:59:50 +0900

    fix(report): N203에 대해서만 서술형 결과 생성하고 나머지는 '-'처리

--- report/src/main/kotlin/com/greencross/lims/service/report/ReportMapper.kt
+++ report/src/main/kotlin/com/greencross/lims/service/report/ReportMapper.kt
@@ -50,7 +50,7 @@ class ReportMapper(private val om: ObjectMapper) {
                   "result": "$result", 
                   "result_type": "$prediction", 
                   "comment": "$comment", 
-                  "text_report":  "${createTextReport(result, prediction, comment)}"
+                  "text_report":  "${createTextReport(analysis.service, result, prediction, comment)}"
                 }    
                 """.trimIndent()
 
@@ -68,23 +68,24 @@ class ReportMapper(private val om: ObjectMapper) {
             "OV" -> "난소암"
             else -> "오류"
     }
-    private fun createTextReport(result: String, predictedCancer: String, comment: String): String {
+    private fun createTextReport(service: String, result: String, predictedCancer: String, comment: String): String {
         val sb = StringBuilder()
-        val guideline = when(result) {
-            "일반관리" -> "-"
-            "관심관리" -> "아이캔서치 검사 모니터링 권장 기간 : 3개월 후"
-            "집중관리" -> "정밀검사 : 주치의와 상담요함 / 아이캔서치 검사 모니터링 권장 기간 : 3개월 후"
-            else -> "오류"
-        }
-        sb.append("◆ 아이캔서치 분석 리포트\n\n")
-        sb.append("■ 종합결과 : $result\n")
-        sb.append("■ 이상 패턴 검출 여부 : ${if(result == "일반관리") "미검출" else "검출"}\n")
-        sb.append("■ 6종 암 중 인공지능 예측 암종 : ${if(result != "집중관리") "해당없음" else predictedCancer}\n")
-        sb.append("■ 기타 소견 : ${if(comment.trim() == "") "-" else comment}\n")
-        sb.append("■ 가이드라인 : $guideline\n")
-        sb.append("■ 검사자 : 김다솜 / 확인자 : 조은해\n")
+        if(service == "N203") {
+            val guideline = when(result) {
+                "일반관리" -> "-"
+                "관심관리" -> "아이캔서치 검사 모니터링 권장 기간 : 3개월 후"
+                "집중관리" -> "정밀검사 : 주치의와 상담요함 / 아이캔서치 검사 모니터링 권장 기간 : 3개월 후"
+                else -> "오류"
+            }
+            sb.append("◆ 아이캔서치 분석 리포트\n\n")
+            sb.append("■ 종합결과 : $result\n")
+            sb.append("■ 이상 패턴 검출 여부 : ${if (result == "일반관리") "미검출" else "검출"}\n")
+            sb.append("■ 6종 암 중 인공지능 예측 암종 : ${if (result != "집중관리") "해당없음" else predictedCancer}\n")
+            sb.append("■ 기타 소견 : ${if (comment.trim() == "") "-" else comment}\n")
+            sb.append("■ 가이드라인 : $guideline\n")
+            sb.append("■ 검사자 : 김다솜 / 확인자 : 조은해\n")
 
-        return sb.toString().replace("\n", "\\n").replace("\"", "\\\"")
+            return sb.toString().replace("\n", "\\n").replace("\"", "\\\"")
+        } else return "-"
     }
-
 }
