a2ea750bb77a6f6e0f6c310eef279b6951ee4de5
mjjuu <mjju@gcgenome.com>
2024-12-09 13:15:15 +0900

    fix(report): 검사자 확인자 추가

--- report/src/main/kotlin/com/greencross/lims/service/report/ReportMapper.kt
+++ report/src/main/kotlin/com/greencross/lims/service/report/ReportMapper.kt
@@ -81,7 +81,8 @@ class ReportMapper(private val om: ObjectMapper) {
         sb.append("■ 이상 패턴 검출 여부 : ${if(result == "일반관리") "미검출" else "검출"}\n")
         sb.append("■ 6종 암 중 인공지능 예측 암종 : ${if(result != "집중관리") "해당없음" else predictedCancer}\n")
         sb.append("■ 기타 소견 : ${if(comment == "") "-" else comment}\n")
-        sb.append("■ 가이드라인 : $guideline")
+        sb.append("■ 가이드라인 : $guideline\n")
+        sb.append("■ 검사자 : 김다솜 / 확인자 : 조은해\n")
 
         return sb.toString().replace("\n", "\\n").replace("\"", "\\\"")
     }
