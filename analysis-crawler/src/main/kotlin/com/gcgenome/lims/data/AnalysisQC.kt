93b3bb47eee7f4aab76ff0d6c4412e792ba692cd
seeun <baese@gcgenome.com>
2025-03-26 16:56:05 +0900

    Revert "fix(cralwer): Nullable 처리"
    This reverts commit 2dfd442153aab30bb11d85122baa01a1695abd72.

--- analysis-crawler/src/main/kotlin/com/gcgenome/lims/data/AnalysisQC.kt
+++ analysis-crawler/src/main/kotlin/com/gcgenome/lims/data/AnalysisQC.kt
@@ -3,31 +3,59 @@ package com.gcgenome.lims.data
 import com.gcgenome.file_reader.FileColumn
 
 data class AnalysisQC(
-    @FileColumn("primary_key")                  var primary:                String  = "",
-    @FileColumn("FREEMIX_fcA")                  var freeMixFcA:             Double? = 0.0,
-    @FileColumn("FREEMIX_fcB")                  var freeMixFcB:             Double? = 0.0,
-    @FileColumn("dup_rate_fcA")                 var dupRateFcA:             Double? = 0.0,
-    @FileColumn("dup_rate_fcB")                 var dupRateFcB:             Double? = 0.0,
-    @FileColumn("GC_fcA")                       var gcFcA:                  Double? = 0.0,
-    @FileColumn("GC_fcB")                       var gcFcB:                  Double? = 0.0,
-    @FileColumn("raw_reads(millions)_fcA")      var rawReadsMillFcA:        Double? = 0.0,
-    @FileColumn("raw_reads(millions)_fcB")      var rawReadsMillFcB:        Double? = 0.0,
-    @FileColumn("filtered_reads(millions)_fcA") var filteredReadsMillFcA:   Double? = 0.0,
-    @FileColumn("filtered_reads(millions)_fcB") var filteredReadsMillFcB:   Double? = 0.0,
-    @FileColumn("mean_is_fcA")                  var meanFcA:                Double? = 0.0,
-    @FileColumn("mean_is_fcB")                  var meanFcB:                Double? = 0.0,
-    @FileColumn("median_is_fcA")                var medianFcA:              Double? = 0.0,
-    @FileColumn("median_is_fcB")                var medianFcB:              Double? = 0.0,
-    @FileColumn("QC_fcA")                       var qcFcA:                  String? = "",
-    @FileColumn("QC_fcB")                       var qcFcB:                  String? = "",
-    @FileColumn("chrX_count_fcA")               var chrXCntFcA:             Long?   = 0,
-    @FileColumn("chrY_count_fcA")               var chrYCntFcA:             Long?   = 0,
-    @FileColumn("chrX_proportion_fcA")          var chrXPropFcA:            Double? = 0.0,
-    @FileColumn("chrY_proportion_fcA")          var chrYPropFcA:            Double? = 0.0,
-    @FileColumn("pred_sex_fcA")                 var predSexFcA:             String? = "",
-    @FileColumn("chrX_count_fcB")               var chrXCntFcB:             Long?   = 0,
-    @FileColumn("chrY_count_fcB")               var chrYCntFcB:             Long?   = 0,
-    @FileColumn("chrX_proportion_fcB")          var chrXPropFcB:            Double? = 0.0,
-    @FileColumn("chrY_proportion_fcB")          var chrYPropFcB:            Double? = 0.0,
-    @FileColumn("pred_sex_fcB")                 var predSexFcB:             String? = ""
+    @FileColumn(name = "primary_key")
+    var primary:                String = "",
+    @FileColumn("FREEMIX_fcA")
+    var freeMixFcA:             Double = 0.0,
+    @FileColumn("FREEMIX_fcB")
+    var freeMixFcB:             Double = 0.0,
+    @FileColumn("dup_rate_fcA")
+    var dupRateFcA:             Double = 0.0,
+    @FileColumn("dup_rate_fcB")
+    var dupRateFcB:             Double = 0.0,
+    @FileColumn("GC_fcA")
+    var gcFcA:                  Double = 0.0,
+    @FileColumn("GC_fcB")
+    var gcFcB:                  Double = 0.0,
+    @FileColumn("raw_reads(millions)_fcA")
+    var rawReadsMillFcA:        Double = 0.0,
+    @FileColumn("raw_reads(millions)_fcB")
+    var rawReadsMillFcB:        Double = 0.0,
+    @FileColumn("filtered_reads(millions)_fcA")
+    var filteredReadsMillFcA:   Double = 0.0,
+    @FileColumn("filtered_reads(millions)_fcB")
+    var filteredReadsMillFcB:   Double = 0.0,
+    @FileColumn("mean_is_fcA")
+    var meanFcA:                Double = 0.0,
+    @FileColumn("mean_is_fcB")
+    var meanFcB:                Double = 0.0,
+    @FileColumn("median_is_fcA")
+    var medianFcA:              Double = 0.0,
+    @FileColumn("median_is_fcB")
+    var medianFcB:              Double = 0.0,
+    @FileColumn("QC_fcA")
+    var qcFcA:                  String = "",
+    @FileColumn("QC_fcB")
+    var qcFcB:                  String = "",
+    @FileColumn("chrX_count_fcA")
+    var chrXCntFcA:             Long = 0,
+    @FileColumn("chrY_count_fcA")
+    var chrYCntFcA:             Long = 0,
+    @FileColumn("chrX_proportion_fcA")
+    var chrXPropFcA:            Double = 0.0,
+    @FileColumn("chrY_proportion_fcA")
+    var chrYPropFcA:            Double = 0.0,
+    @FileColumn("pred_sex_fcA")
+    var predSexFcA:             String = "",
+    @FileColumn("chrX_count_fcB")
+    var chrXCntFcB:             Long = 0,
+    @FileColumn("chrY_count_fcB")
+    var chrYCntFcB:             Long = 0,
+    @FileColumn("chrX_proportion_fcB")
+    var chrXPropFcB:            Double = 0.0,
+    @FileColumn("chrY_proportion_fcB")
+    var chrYPropFcB:            Double = 0.0,
+    @FileColumn("pred_sex_fcB")
+    var predSexFcB:             String = ""
+
 )
--- analysis-crawler/src/main/kotlin/com/gcgenome/lims/data/AnalysisResult.kt
+++ analysis-crawler/src/main/kotlin/com/gcgenome/lims/data/AnalysisResult.kt
@@ -3,13 +3,21 @@ package com.gcgenome.lims.data
 import com.gcgenome.file_reader.FileColumn
 
 data class AnalysisResult(
-    @FileColumn("primary_key")                  var primary:            String  = "",
-    @FileColumn("CAD_ensemble_prob")            var cadEnsembleProb:    Double? = 0.0,
-    @FileColumn("TOO5_pred_class")              var too5Pred:           String? = "",
-    @FileColumn("TOO5_rmd_fems_ensemble_prob")  var too5Fems:           Double? = 0.0,
-    @FileColumn("TOO6_pred_class")              var too6Pred:           String? = "",
-    @FileColumn("TOO6_rmd_fems_ensemble_prob")  var too6Fems:           Double? = 0.0,
-    @FileColumn("Iscore_v1.6.4")                var iscore:             Double? = 0.0,
-    @FileColumn("results")                      var result:             String? = ""
+    @FileColumn("primary_key")
+    var primary:            String = "",
+    @FileColumn("CAD_ensemble_prob")
+    var cadEnsembleProb:    Double = 0.0,
+    @FileColumn("TOO5_pred_class")
+    var too5Pred:           String = "",
+    @FileColumn("TOO5_rmd_fems_ensemble_prob")
+    var too5Fems:           Double = 0.0,
+    @FileColumn("TOO6_pred_class")
+    var too6Pred:           String = "",
+    @FileColumn("TOO6_rmd_fems_ensemble_prob")
+    var too6Fems:           Double = 0.0,
+    @FileColumn("Iscore_v1.6.4")
+    var iscore:             Double = 0.0,
+    @FileColumn("results")
+    var result:             String = ""
 ) {
 }
