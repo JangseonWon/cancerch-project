f6366fd4e652298d2fae219ba935e59f1278d0da
mjjuu <mjjuu@gcgenome.com>
2025-06-16 13:42:19 +0900

    fix(publish): publisher 버전 변경

--- publish/build.gradle.kts
+++ publish/build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
     implementation(libs.bundles.r2dbc.querydsl)
     implementation(libs.spring.kafka)
     implementation("com.gcgenome:alis-report-api:1.1.3")
-    implementation("com.gcgenome:workflow-publisher:1.3.2")
+    implementation("com.gcgenome:workflow-publisher:2025.3.19-SPRING2.x")
     kapt(libs.bundles.r2dbc.querydsl)
     testImplementation(libs.bundles.test)
 }
